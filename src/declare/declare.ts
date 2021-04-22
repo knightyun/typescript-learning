716c06cb0183211147543cef9e844b26fd54c6bf
knightyun <2386209384@qq.com>
2021-04-22 10:16:34 +0800

    Add declare notes

---
+++ src/declare/declare.ts
@@ -0,0 +1,59 @@
+/************************************************************
+ * 使用声明
+ *
+ ************************************************************/
+
+/**
+ * 声明引入指令
+ */
+// 该指令表示对依赖的引用；
+// 解析包的机制与 import 类似，可以当作用于导入 声明包；
+// 一般在声明文件中使用该指令，可以使用相对或非相对引用，
+/// <reference types="./demo" />
+// 上面的指令会寻找 ./demo.d.ts 或 ./demo/index.d.ts 声明文件
+
+// 下面的指令表示引用 node 的声明包：
+// /// <reference types="node" />
+// 该指令回去寻找 node_modules/@types/node/index.d.ts 声明文件
+
+
+/** 
+ * 使用引入的声明文件中定义的变量
+ */
+
+// 访问全局变量
+dNum = 1;
+
+// 声明和使用也可以同时在一个文件
+declare let dNum1: number;
+dNum1 = 2;
+
+// 访问全局声明函数
+dFn(dNum);
+
+// 访问命名空间中的变量（把命名空间当成对象访问），
+// 以及命名空间中的命名空间
+DName.fn(DName.dNum, DName.DName1.dNum1);
+
+// 访问函数重载
+dFn2('abc');
+dFn2(123);
+
+// 访问声明接口
+const dObj: IDeclare = {
+  str: 'abc',
+  bol: true,
+}
+
+// 访问声明类型
+let dVar: DType = 'abc';
+dVar = 123;
+
+// 访问声明类
+class DCS1 extends DCS {
+  constructor(arg: string) {
+    super(arg);
+  }
+}
+const dcs1 = new DCS1('abc');
+
---
+++ src/declare/demo/index.d.ts
@@ -0,0 +1,63 @@
+/************************************************************
+ * 声明文件
+ *
+ * ts 中声明文件都以 .d.ts 作为后缀
+ ************************************************************/
+
+// 声明 全局变量
+declare var dNum: number;
+
+// 声明可以带有初始值
+// declare var dNum: number = 1;
+
+// 声明 只读变量（常量）
+declare const dStr: string;
+
+// 声明 块级作用域变量（如果存在块）
+declare let dBol: boolean;
+
+// 全局函数
+declare function dFn(arg: number): number;
+
+// 申明也可以包含具体实现
+declare function dFn1(arg: number): number {
+  return arg;
+};
+
+// 带有命名空间的变量
+declare namespace DName {
+  // 内部变量不会与全局冲突
+  // 直接写变量申明，不用再加 declare
+  let dNum: number;
+
+  // 使用嵌套的命名空间
+  namespace DName1 {
+    let dNum1: number;
+  }
+
+  function fn(arg: number, arg2: number): number { return arg + arg2; }
+}
+
+// 声明函数重载
+declare function dFn2(arg: string): boolean;
+declare function dFn2(arg: number): string;
+
+// 声明接口
+declare interface IDeclare {
+  str: string;
+  num?: number;
+  readonly bol: boolean;
+}
+
+// 声明类型
+declare type DType = string | number;
+
+// 声明类
+declare class DCS {
+  protected dVar: string;
+
+  constructor(arg: string) {
+    this.dVar = arg;
+  }
+}
+
